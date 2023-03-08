05b591d0f13c84d5172841fadcf9c6680350e8f4
LukeBromley <46723068+LukeBromley@users.noreply.github.com>
2023-03-08 13:50:17 +0000

    Machine learning management iteration loop

--- machine_learning/machine_learning_manager.py
+++ machine_learning/machine_learning_manager.py
@@ -3,24 +3,35 @@ if system() == 'Windows':
     import sys
     sys.path.append('./')
 import os
+import csv
 from library.file_management import FileManagement
 from machine_learning.deep_q_learning import MachineLearning
 
 class MachineLearningManager:
     def __init__(self, file_path) -> None:
-        self.machine_learning_options = FileManagement().load_ML_configs_from_file(self.machine_learning_config)
-        self.iterations = self.get_iteration(file_path)
+        self.iterations = []
+        self.get_iterations(file_path)
+        self.run_iterations()
 
-    def get_iteration(self, file_path):
-        pass 
-    
+    def get_iterations(self, file_path):
+        with open(file_path, "r") as file:
+            csv_reader = csv.DictReader(file)
+            for file_dict in csv_reader:
+                self.iterations.append(file_dict)
+
     def run_iterations(self):
         for iteration in self.iterations:
-            self.run_iteration(iteration["Junction"], iteration["SimulationConfig"], iteration["MachineLearningConfig"], iteration["MAchineLearningConfigID"])
+            self.run_iteration(iteration["Junction"], iteration["SimulationConfig"], iteration["MachineLearningConfig"], iteration["MachineLearningConfigID"])
     
     def run_iteration(self, junction_file_name, sim_config_file, machine_learning_config_file, machine_learning_config_id):
         junction_file_path = os.path.join(os.path.dirname(os.path.join(os.path.dirname(__file__))), "junctions", junction_file_name)
         simulation_config_file_path = os.path.join(os.path.dirname(os.path.join(os.path.dirname(__file__))), "configurations", "simulation_config", sim_config_file)
-        machine_learning_options = FileManagement().load_ML_configs_from_file(machine_learning_config_file)
-        machine_learning_config = machine_learning_options[machine_learning_config_id]
+        machine_learning_file = os.path.join(os.path.dirname(os.path.join(os.path.dirname(__file__))),"configurations", "machine_learning_config", machine_learning_config_file)
+        machine_learning_options = FileManagement().load_ML_configs_from_file(machine_learning_file)
+        machine_learning_config = machine_learning_options[int(machine_learning_config_id)]
+        print("\n\nMachine Learning with")
+        print("Junction:", junction_file_path)
+        print("SimConfig:", simulation_config_file_path)
+        print("ML:", machine_learning_config.config_id)
         machine_learning = MachineLearning(junction_file_path, simulation_config_file_path, machine_learning_config)
+        #machine_learning.train()
