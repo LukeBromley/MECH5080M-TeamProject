8c25ac3176be97ee436eaf6593da649ed3d98ee5
vilius <viliusmazk@gmail.com>
2023-04-18 13:31:32 +0300

    random freeze time

--- simulation/junction/j_simulation_manager.py
+++ simulation/junction/j_simulation_manager.py
@@ -55,7 +55,7 @@ class SimulationManager:
 
     def reset(self):
         self.simulation = self.create_simulation()
-        self.freeze_traffic(100)
+        self.freeze_traffic()
         return self.get_state()
 
     def freeze_traffic(self, n: int = None):
