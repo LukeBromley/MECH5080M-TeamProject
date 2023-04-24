ed89ba3a86e5a79c1a8650db8ea248a723f5ac40
LukeBromley <46723068+LukeBromley@users.noreply.github.com>
2023-04-24 15:05:09 +0100

    Results csv generated

--- analysis_tools/results_gatherer.py
+++ analysis_tools/results_gatherer.py
@@ -1,5 +1,6 @@
 import os
 import json
+import results_spreadsheet_maker
 
 results_location = "../results/legacy_control_even_spawning_TESTED(4)"
 run_folders = [x[0] for x in os.walk(results_location)]
@@ -10,15 +11,6 @@ for folder in run_folders[1:]:
     with open(folder + "/testing_run_parameters.json", "r") as file:
         all_results.append(json.load(file))
 
-all_results_per_cpm = {}
-
-for result in all_results:
-    cpm = int(result["RunUID"].split("_")[-1][:-3])
-
-    if str(cpm) in all_results_per_cpm.keys():
-        all_results_per_cpm[str(cpm)].append(result)
-    else:
-        all_results_per_cpm[str(cpm)] = [result]
-
+results_spreadsheet_maker.main(all_results)
 
 print()
