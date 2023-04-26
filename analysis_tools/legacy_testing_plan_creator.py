21fb995e2d7f8875550a82a8973d6d1c9d3c99ad
Henry Triff <henry.w.t.3@gmail.com>
2023-04-26 15:04:58 +0100

    Created Test Plans

--- analysis_tools/legacy_testing_plan_creator.py
+++ analysis_tools/legacy_testing_plan_creator.py
@@ -1,5 +1,7 @@
 import json
 
+# 100% Autonomous - 0% Latency - 0% Packet Loss - Even Spawning
+
 save_plan_file_name = "../testing_plans/legacy_control_even_spawning.plan"
 
 control_methods = ["fixed timings", "demand scheduling"]
@@ -33,16 +35,12 @@ plan = {}
 uid = 0
 
 for control_method in control_methods:
-    print(control_method)
     for index, junction in enumerate(junctions):
-        print(junction)
         for random_seed_value in random_seed_values:
-            print(random_seed_value)
             for spawn_time_delta_value in spawn_time_delta_values:
-                print(spawn_time_delta_value)
 
                 plan[str(uid)] = {
-                    "RunUID": "legacy_" + control_method.replace(" ", "_") + "_" + junction[:-5] + "_" + str(random_seed_value) + "_" + str(round(60/spawn_time_delta_value, 2)) + "cpm",
+                    "RunUID": "Legacy_even_spawning_control_method_" + control_method.replace(" ", "_") + "_junction_" + junction[:-5] + "_seed_" + str(random_seed_value) + "_spawn_rate_" + str(round(60 / spawn_time_delta_value, 2)) + "cpm",
                     "RunType": control_method,
                     "Junction": junction,
                     "SimulationConfig": "final_testing/even_spawning/autonomous/seed_" + str(random_seed_value) + "/" + str(round(60/spawn_time_delta_value, 2)) + "cpm.config",
@@ -58,3 +56,63 @@ for control_method in control_methods:
 
 with open(save_plan_file_name, "w") as file:
     json.dump(plan, file)
+
+# 100% Autonomous - 0% Latency - 0% Packet Loss - Uneven Spawning
+
+save_plan_file_name = "../testing_plans/legacy_control_uneven_spawning.plan"
+
+control_methods = ["fixed timings", "demand scheduling"]
+junctions = ["simple_T_junction.junc", "simple_X_junction.junc"]
+config_folders = [["simple_T/bottom/", "simple_T/side/"], ["simple_X/single/", "simple_X/double/"]]
+random_seed_values = [1788621521, 1774553582, 1490597230, 997415346, 1433874439]
+spawn_time_delta_values = [90, 60, 45, 30, 20, 15, 10, 5, 4, 3, 2]
+steps = 4500
+
+actions = [
+    [2, 1],
+    [2, 1, 4],
+    [2, 1, 8, 4],
+    [35, 56, 22]
+]
+
+action_durations = [
+    [300, 300],
+    [300, 300, 300],
+    [300, 300, 300, 300],
+    [300, 300, 300]
+]
+
+action_paths = [
+    [[5], [7]],
+    [[3], [5], [10]],
+    [[16], [18], [20], [13]],
+    [[7, 8], [10, 11], [13, 14]]
+]
+
+plan = {}
+uid = 0
+
+for control_method in control_methods:
+    for index, junction in enumerate(junctions):
+        for config_folder in config_folders[index]:
+            for random_seed_value in random_seed_values:
+                for spawn_time_delta_value in spawn_time_delta_values:
+                    plan[str(uid)] = {
+                        "RunUID": "Legacy_uneven_spawning_control_method_" + control_method.replace(" ", "_") + "_junction_" + junction[:-5] + "_seed_" + str(random_seed_value) + "_spawn_rate_" + str(round(60/spawn_time_delta_value, 2)) + "cpm",
+                        "RunType": control_method,
+                        "Junction": junction,
+                        "SimulationConfig": "final_testing/uneven_spawning/" + config_folder + "seed_" + str(random_seed_value) + "/" + str(round(60/spawn_time_delta_value, 2)) + "cpm.config",
+                        "Steps": steps,
+                        "Actions": actions[index],
+                        "ActionDurations": action_durations[index],
+                        "ActionPaths": action_paths[index],
+                        "RandomSeed": random_seed_value,
+                        "CPM": (round(60/spawn_time_delta_value, 2))
+                    }
+
+                    uid += 1
+
+with open(save_plan_file_name, "w") as file:
+    json.dump(plan, file)
+
+
